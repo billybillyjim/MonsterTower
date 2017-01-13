18080193e59a2e2ddc691c11e0856374c0345fcb
Luke <thereallmwels@gmail.com>
2017-01-13 09:19:21 +0900

    Added Faction Outline

---
+++ Assets/Code/Faction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Faction : MonoBehaviour {
+
+    private string title;
+    private float prestige;
+    private float relations;
+
+	public void setTitle(string s)
+    {
+        title = s;
+    }
+    public void setPrestige(float p)
+    {
+        prestige = p;
+    }
+    public void addPrestige(float p)
+    {
+        prestige += p;
+    }
+    public void setRelations(float r)
+    {
+        relations = r;
+    }
+    public void addRelations(float r)
+    {
+        relations += r;
+    }
+    public float getPrestige()
+    {
+        return prestige;
+    }
+}
---
+++ Assets/Code/FactionManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FactionManager : MonoBehaviour {
+
+    private List<Faction> factions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	
+
+}
